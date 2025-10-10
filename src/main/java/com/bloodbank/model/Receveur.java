3baa46ef6caa9c1cf12b8877d04ddb451eb22274
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-10-10 11:17:58 +0100

    Ajout des attributs besoin, pochesRequises, pochesRecues et état avec annotations Enum à la classe Receveur

--- src/main/java/com/bloodbank/model/Receveur.java
+++ src/main/java/com/bloodbank/model/Receveur.java
@@ -1,4 +1,37 @@
 package com.bloodbank.model;
 
+import com.bloodbank.model.enums.EtatReceveur;
+import com.bloodbank.model.enums.NiveauBesoin;
+
+import javax.persistence.CascadeType;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Receveur {
+
+
+    @Enumerated(EnumType.STRING)
+    private NiveauBesoin besoin;
+
+
+    private int pochesRequises;
+    private int pochesRecues;
+
+
+    @Enumerated(EnumType.STRING)
+    private EtatReceveur etat;
+
+    @OneToMany(mappedBy = "receveur" ,cascade = CascadeType.ALL)
+    private List<DonationAssociation> associations = new ArrayList<>();
+
+
+
+
+
+
+
+
 }
