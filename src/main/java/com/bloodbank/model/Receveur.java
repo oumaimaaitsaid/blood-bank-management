96866d598cb56e7f39f75132e6051375d5d8e8db
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-10-10 11:18:40 +0100

    Implémentation complète de la classe Receveur avec relations, énumérations et méthodes utilitaires getter/setters

--- src/main/java/com/bloodbank/model/Receveur.java
+++ src/main/java/com/bloodbank/model/Receveur.java
@@ -28,10 +28,37 @@ public class Receveur {
     private List<DonationAssociation> associations = new ArrayList<>();
 
 
+    // ----Méthodes Util ----
+
+
+    public boolean isSatisfait()
+    {
+        return pochesRecues >= pochesRequises;
+    }
+
+    public void ajouterPoche(){
+
+        this.pochesRecues++;
+        if(isSatisfait()) this.etat=EtatReceveur.SATISFAIT;
+    }
 
 
 
+    // ---- Getters & Setters ----
+    public NiveauBesoin getBesoin() { return besoin; }
+    public void setBesoin(NiveauBesoin besoin) { this.besoin = besoin; }
 
+    public int getPochesRequises() { return pochesRequises; }
+    public void setPochesRequises(int pochesRequises) { this.pochesRequises = pochesRequises; }
+
+    public int getPochesRecues() { return pochesRecues; }
+    public void setPochesRecues(int pochesRecues) { this.pochesRecues = pochesRecues; }
+
+    public EtatReceveur getEtat() { return etat; }
+    public void setEtat(EtatReceveur etat) { this.etat = etat; }
+
+    public List<DonationAssociation> getAssociations() { return associations; }
+    public void setAssociations(List<DonationAssociation> associations) { this.associations = associations; }
 
 
 }
