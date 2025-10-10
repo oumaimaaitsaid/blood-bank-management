0b3a8b75306604d8bc72f5bc95892880092b3aad
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-10-10 11:28:21 +0100

    Ajout des getters et setters pour tous les attributs de la classe DonationAssociation

--- src/main/java/com/bloodbank/model/DonationAssociation.java
+++ src/main/java/com/bloodbank/model/DonationAssociation.java
@@ -27,4 +27,19 @@ public class DonationAssociation {
     private String statut ="CONFIRMÉ";
 
 
+    // ---- Getters & Setters ----
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
+    public Donneur getDonneur() { return donneur; }
+    public void setDonneur(Donneur donneur) { this.donneur = donneur; }
+
+    public Receveur getReceveur() { return receveur; }
+    public void setReceveur(Receveur receveur) { this.receveur = receveur; }
+
+    public LocalDate getDateAssociation() { return dateAssociation; }
+    public void setDateAssociation(LocalDate dateAssociation) { this.dateAssociation = dateAssociation; }
+
+    public String getStatut() { return statut; }
+    public void setStatut(String statut) { this.statut = statut; }
+}
