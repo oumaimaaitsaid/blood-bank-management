f00627af9204960cb48bcb527e5ce050bc7e45bf
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-10-10 11:27:45 +0100

    Ajout des attributs id, dateAssociation et statut avec mapping JPA

--- src/main/java/com/bloodbank/model/DonationAssociation.java
+++ src/main/java/com/bloodbank/model/DonationAssociation.java
@@ -9,7 +9,22 @@ import java.time.LocalDate;
 public class DonationAssociation {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
+    @ManyToOne
+    @JoinColumn(name ="donneur_id")
+    private  Donneur donneur;
+
+    @ManyToOne
+    @JoinColumn(name ="receveur_id")
+    private Receveur receveur;
+
+
+    private LocalDate dateAssociation;
+
+    @Column(nullable =false)
+    private String statut ="CONFIRMÉ";
 
 
 
