90ddc2e850d0fa7fc308188a424b860d51272325
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-10-10 11:27:05 +0100

    git add src/main/java/com/bloodbank/model/DonationAssociation.java

--- src/main/java/com/bloodbank/model/DonationAssociation.java
+++ src/main/java/com/bloodbank/model/DonationAssociation.java
@@ -1,4 +1,15 @@
 package com.bloodbank.model;
 
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.*;
+import java.time.LocalDate;
+
 public class DonationAssociation {
-}
+
+    @Id
+
+
+
+
