49f8962a171633d98f422abbd7d6623e06f1b3fe
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-10-13 14:06:58 +0100

    ✨ Ajout de la servlet DonneurServlet pour la gestion des donneurs (liste, ajout, suppression)

---
+++ src/main/java/com/bloodbank/servlet/DonneurServlet.java
@@ -0,0 +1,24 @@
+package  com.bloodbank.servlet;
+
+
+import com.bloodbank.model.Donneur;
+import com.bloodbank.model.enums.Sexe;
+import com.bloodbank.service.DonneurService;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class DonneurServlet extends HttpServlet {
+
+
+    private DonneurService service =new DonneurService();
+
+
+
+
+
+
+}
