e06dd82346f903094c0ad69f719e45e66584c235
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-10-13 11:37:29 +0100

    ajouter methode ajouterDonneur en DonneurService

--- src/main/java/com/bloodbank/service/DonneurService.java
+++ src/main/java/com/bloodbank/service/DonneurService.java
@@ -1,18 +1,29 @@
 package com.bloodbank.service;
 
 
+import com.bloodbank.dao.DonneurDAO;
 import com.bloodbank.model.Donneur;
 import com.bloodbank.model.Personne;
+import com.bloodbank.model.enums.Disponibilite;
 
 import java.time.LocalDate;
 import java.time.Period;
 
 public class DonneurService{
-
+    private DonneurDAO donneurDAO = new DonneurDAO();
     private boolean isEligible(Donneur d){
         int age = Period.between(d.getDateNaissance() , LocalDate.now()).getYears();
         boolean ageOK = age >=18 && age >=65;
         boolean poidsOK = d.getPoids() >= 50;
         return ageOK && poidsOK;
+    }
+
+    public void ajouterDonneur(Donneur d){
+        if(!isEligible(d)){
+            d.setStatus(Disponibilite.NON_ELIGIBLE);
+        } else {
+            d.setStatus(Disponibilite.DISPONIBLE);
+        }
+        donneurDAO.save(d);
     }
 }
