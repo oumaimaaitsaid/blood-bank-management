9b286b2621bf9d8054a620bfb3539c8bee112aa5
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-10-14 14:59:41 +0100

    ajout de la logique doGet pour lister, supprimer et afficher le formulaire des receveurs

--- src/main/java/com/bloodbank/servlet/ReceveurServlet.java
+++ src/main/java/com/bloodbank/servlet/ReceveurServlet.java
@@ -1,4 +1,41 @@
 package com.bloodbank.servlet;
 
-public class ReceveurServlet {
+import com.bloodbank.model.Receveur;
+import com.bloodbank.model.enums.GroupeSanguin;
+import com.bloodbank.model.enums.NiveauBesoin;
+import com.bloodbank.model.enums.Sexe;
+import com.bloodbank.service.ReceveurService;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+
+
+public class ReceveurServlet  extends  HttpServlet{
+
+    private ReceveurService service =new ReceveurService();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws  ServletException,IOException{
+        String action = request.getParameter("action");
+
+        if("list".equals(action)){
+            request.setAttribute("receveurs", service.listerReceveurs());
+            RequestDispatcher rd =request.getRequestDispatcher("receveur-list.jsp");
+            rd.forward(request,response);
+
+        } else if("delete".equals(action)){
+            int id = Integer.parseInt(request.getParameter("id"));
+            service.supprimer(id);
+            response.sendRedirect("receveur?action=list");
+        }else {
+            request.setAttribute("groupes" ,Arrays.asList(GroupeSanguin.values()));
+            request.setAttribute("besoins" ,Arrays.asList(NiveauBesoin.values()));
+            RequestDispatcher rd = request.getRequestDispatcher("receveur-form.jsp");
+            rd.forward(request, response);
+        }
+    }
 }
